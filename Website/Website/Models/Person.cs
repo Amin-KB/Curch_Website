12411f4234590bc86466f88029b0f4b05f7fdacd
Amin-KB <aminkarambeigi94@gmail.com>
2022-08-15 11:57:57 +0200

    Role and Claim

--- Website/Website/Models/Person.cs
+++ Website/Website/Models/Person.cs
@@ -1,6 +1,19 @@
-﻿namespace Website.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Website.Models
 {
     public class Person
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PersonId { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public Guid Guid { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Role { get; set; } = 2;
+        public bool Active { get; set; } = true;
+
     }
 }
--- Website/Website/Program.cs
+++ Website/Website/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).
+    AddCookie(x => x.LoginPath = "/Auth/LogIn");
 
 var app = builder.Build();
 
@@ -15,10 +19,13 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseCookiePolicy();
 
 app.UseRouting();
 
 app.UseAuthorization();
+app.UseAuthentication();
+
 
 app.MapControllerRoute(
     name: "default",
